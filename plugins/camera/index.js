3622c211a1d7688eff8fff5fbb3f2ed88e64cd70
David Gray <grayda@solidinc.org>
2016-09-21 02:14:06 +1000

    Added camera functions

--- plugins/camera/index.js
+++ plugins/camera/index.js
@@ -1,12 +1,29 @@
 module.exports = function setup(options, imports, register) {
-  var package = require("./package.json")
-  var fs = require("fs")
+    var package = require("./package.json")
+    var fs = require("fs")
+    var RaspiCam = require("raspicam");
+    var cam = new RaspiCam({
+        ts: 300000,
+        mode: "timelapse",
+        output: process.cwd() + "/images/image.jpg",
+        ex: "auto"
+    });
 
-  cameraObj = {
-    plugin: package
-  }
+    camera.on("read", function(err, timestamp, filename){
+      imports.eventbus.emit("camera.photo.taken", filename)
+    });
 
-  register(null, {
-    camera: cameraObj
-  });
+    cameraObj = {
+        plugin: package,
+        start: function() {
+          cam.start()
+        },
+        stop: function() {
+          cam.stop()
+        }
+    }
+
+    register(null, {
+        camera: cameraObj
+    });
 };
