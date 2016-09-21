370e8d0b6d62670b40684ae0e4d23d51b9cd0ff2
David Gray <grayda@solidinc.org>
2016-09-22 03:38:53 +1000

    Camera plugin now watermarks

--- plugins/camera/index.js
+++ plugins/camera/index.js
@@ -5,11 +5,11 @@ module.exports = function setup(options, imports, register) {
     cameraObj = {
         plugin: package,
         start: function() {
-            cp.exec("/opt/vc/bin/raspistill --timelapse 60000 --quality 100 --width 1440 --height 900 --output " + process.cwd() + "/images/image.jpg --exposure auto -a 4 -a \"" + imports.config.config.location + " %Y-%m-%d %X%z" + (new Date()).getTimezoneOffset() + "\"")
+            cp.exec("/opt/vc/bin/raspistill --timelapse 60000 --quality 100 --width 1440 --height 900 --output " + process.cwd() + "/images/image.jpg --exposure auto -a 4 -a \"" + imports.config.config.location + " %Y-%m-%d %X%z\"")
             fs.watch(process.cwd() + "/images/", function(eventType, filename) {
                 if (filename === "image.jpg") {
-                    console.log("Image found. Uploading")
-                    imports.uploader.upload(process.cwd() + "/images/image.jpg")
+                    console.log("Image found. Watermarking")
+                    imports.imagemagick.watermark(process.cwd() + "/images/image.jpg")
                 }
             });
         },
