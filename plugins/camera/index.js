e4e27d1dc3a0a82edca6f0adf7ee8712bf4a5186
David Gray <grayda@solidinc.org>
2016-09-21 17:18:12 +1000

    Removing dependency on raspicam

--- plugins/camera/index.js
+++ plugins/camera/index.js
@@ -1,33 +1,16 @@
 module.exports = function setup(options, imports, register) {
     var package = require("./package.json")
     var fs = require("fs")
-    var RaspiCam = require("raspicam");
-    var cam = new RaspiCam({
-        mode: "timelapse",
-        timelapse: 60000,
-        quality: 100,
-        width: 1440,
-        height: 900,
-        output: process.cwd() + "/images/image.jpg",
-        exposure: "auto"
-    });
-
-    cam.on("read", function(err, timestamp, filename){
-      if(filename.indexOf("~") == -1) {
-        imports.uploader.upload(process.cwd() + "/images/" + filename)
-      }
-    });
-
-    cam.on("start", function(err, timestamp){
-      if(err) {
-        console.dir(err)
-      }
-    });
-
+    var cp = require("child_process")
     cameraObj = {
         plugin: package,
         start: function() {
-          cam.start()
+          cp.exec("/opt/vc/bin/raspistill --timelapse 60000 --quality 100 --width 1440 --height 900 --output " + process.cwd() + "/images/image.jpg --exposure auto")
+          fs.watch(process.cwd() + "/images/", function(eventType, filename) {
+            if (filename)
+            console.log(filename);
+            // Prints: <Buffer ...>
+          });
         },
         stop: function() {
           cam.stop()
