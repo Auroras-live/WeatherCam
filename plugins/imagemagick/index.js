63baa5860fd15f6026615788931570c69b4b0ea0
David Gray <grayda@solidinc.org>
2016-09-22 03:38:45 +1000

    Logo for imagemagick plugin

--- plugins/imagemagick/index.js
+++ plugins/imagemagick/index.js
@@ -1,9 +1,19 @@
 module.exports = function setup(options, imports, register) {
   var package = require("./package.json")
   var fs = require("fs")
+  var gm = require("gm")
 
   imagemagickObj = {
-    plugin: package
+    plugin: package,
+    watermark: function(image) {
+      gm(image).composite(process.cwd() + 'logo.png').write(image, function(err) {
+        if(err) {
+          console.log(err)
+        } else {
+          imports.uploader.upload(process.cwd() + "/images/image.jpg")
+        }
+      })
+    }
   }
 
   register(null, {
