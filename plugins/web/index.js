0097516cbd8d8d8a8802c27e6aede5b01579a691
David Gray <grayda@solidinc.org>
2016-09-22 14:47:51 +1000

    Take a photo on reload

--- plugins/web/index.js
+++ plugins/web/index.js
@@ -10,11 +10,12 @@ module.exports = function setup(options, imports, register) {
 
     // Create server
     var server = http.createServer(function onRequest (req, res) {
+      camera.snap()
       serve(req, res, finalhandler(req, res))
     })
 
     // Listen
-    server.listen(3000)
+    server.listen(9001)
 
     // Define our plugin and functions
     var webObj = {
