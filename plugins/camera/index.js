acc3e5871edeeb58312dc4773b0b7a0b6cb70e3f
David Gray <grayda@solidinc.org>
2016-09-23 00:55:02 +1000

    Added more flip options

--- plugins/camera/index.js
+++ plugins/camera/index.js
@@ -5,14 +5,16 @@ module.exports = function setup(options, imports, register) {
     cameraObj = {
         plugin: package,
         start: function() {
-          if(typoeof imports.config.config.flipImage !== "undefined") {
-            flip = "-vf"
-          } else {
             flip = ""
-          }
+            if (typeof imports.config.config.flip.horizontal !== "undefined") {
+                flip = "-hf"
+            } else if (typeof imports.config.config.flip.vertical !== "undefined") {
+                flip = "-vf"
+            }
+
             cp.exec("/opt/vc/bin/raspistill " + flip + " --timelapse 1000 --quality 100 --width 1440 --height 900 --output " + process.cwd() + "/images/image.jpg -t 999999999 --exposure auto -a 4 -a \"" + imports.config.config.location + " %Y-%m-%d %X%z\"")
             setInterval(function() {
-              imports.imagemagick.watermark(process.cwd() + "/images/image.jpg")
+                imports.imagemagick.watermark(process.cwd() + "/images/image.jpg")
             }, 60000)
         },
         stop: function() {
