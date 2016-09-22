e82842e21ba273b53c7da5eb81e3812b8ca6ef1d
David Gray <grayda@solidinc.org>
2016-09-22 18:08:47 +1000

    Flip, flop & fly!

--- plugins/camera/index.js
+++ plugins/camera/index.js
@@ -5,7 +5,12 @@ module.exports = function setup(options, imports, register) {
     cameraObj = {
         plugin: package,
         start: function() {
-            cp.exec("/opt/vc/bin/raspistill --timelapse 1000 --quality 100 --width 1440 --height 900 --output " + process.cwd() + "/images/image.jpg -t 999999999 --exposure auto -a 4 -a \"" + imports.config.config.location + " %Y-%m-%d %X%z\"")
+          if(typoeof imports.config.config.flipImage !== "undefined") {
+            flip = "-vf"
+          } else {
+            flip = ""
+          }
+            cp.exec("/opt/vc/bin/raspistill " + flip + " --timelapse 1000 --quality 100 --width 1440 --height 900 --output " + process.cwd() + "/images/image.jpg -t 999999999 --exposure auto -a 4 -a \"" + imports.config.config.location + " %Y-%m-%d %X%z\"")
             setInterval(function() {
               imports.imagemagick.watermark(process.cwd() + "/images/image.jpg")
             }, 60000)
