7f11c169c8f7d62df7356b197d25f5e06fcc4969
David Gray <grayda@solidinc.org>
2016-10-03 18:24:15 +1100

    Installer script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#! /bin/sh
+
+# Install git
+sudo apt-get install git
+
+# cd into home
+cd ~/
+
+# Clone Bellboy
+git clone http://github.com/auroras-live/weathercam
+
+# cd into the directory
+cd ~/weathercam
+
+# Get the latest version of node
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash
+
+# Install node
+sudo apt-get install nodejs
+
+# Install everything
+sudo npm install -g gulp nodemon node-autostart
+
+# Install stuff for bellboy
+npm install
+
+# Run the gulp task to set stuff up
+gulp install-plugins
+
+# Set up autostart
+autostart enable -n "weathercam" -p "~/weathercam" -c "nodemon index"
+
+# Set up raspi-config
+sudo raspi-config
+
+sudo grep 'pi /bin/bash /home/pi/weathercam/update.sh' /etc/crontab || sudo echo '0 0 * * * pi /bin/bash /home/pi/weathercam/update.sh' >> /etc/crontab
