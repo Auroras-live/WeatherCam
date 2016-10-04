dbd46ceb4cb47dd4015e354c1636f68bd72c05b3
David Gray <grayda@solidinc.org>
2016-10-04 23:59:13 +1100

    Put weather info on the side as icons
    To match the aesthetics of the embed images and app.

--- plugins/imagemagick/index.js
+++ plugins/imagemagick/index.js
@@ -2,29 +2,39 @@ module.exports = function setup(options, imports, register) {
     var package = require("./package.json")
     var fs = require("fs")
     var gm = require("gm")
+    var moment = require("moment")
     var request = require("request")
+    var fontsize = 30
+    var iconsize = 28
 
     imagemagickObj = {
         plugin: package,
         watermark: function(image) {
+
             imports.logger.log("imagemagick", "Getting weather for " + imports.config.config.lat + ", " + imports.config.config.long)
             weather = request({
                 url: "https://api.auroras.live/v1/?type=all&weather=true&forecast=false&probability=true&threeday=false&ace=false&lat=" + imports.config.config.lat + "&long=" + imports.config.config.long + "&tz=" + (new Date()).getTimezoneOffset(),
                 json: true
             }, function(err, obj, body) {
-                if(err) { imports.logger.log("imagemagick", "Error getting weather. Error was: ", err) }
+                if (err) {
+                    imports.logger.log("imagemagick", "Error getting weather. Error was: ", err)
+                }
                 imports.logger.log("imagemagick", "Weather retrieved", "info", body)
-                imageText = body.weather.location.name + ", " + body.weather.location.state + ", " + body.weather.location.country + " " + body.date + "\nCloud: " + body.weather.cloud + "% | Temp: " + body.weather.temperature + "°C | Rain: " + body.weather.rain + "mm\nLocal aurora probability: " + body.probability.calculated.value + "%"
-                gm(image).font(process.cwd() + "/OpenSans-Regular.ttf").fontSize(25).stroke('black', 1).fill('white').drawText(0, 40, imageText, 'north').stroke('none').fill('white').drawText(0, 40, imageText, 'north').write(process.cwd() + "/images/image_watermark.jpg", function(err) {
-                  imports.logger.log("imagemagick", "Writing text to " + image)
-                    gm(process.cwd() + "/images/image_watermark.jpg").composite(process.cwd() + '/logo.png').gravity("SouthEast").geometry("+25+25").write(process.cwd() + "/images/image_watermark.jpg", function(err) {
-                        imports.logger.log("imagemagick", "Writing watermark to " + process.cwd() + "/images/image_watermark.jpg")
-                        if (err) {
-                            imports.logger.error("imagemagick", err)
-                        } else {
-                            imports.logger.log("imagemagick", "Image watermarked. Proceeding to upload")
-                            imports.uploader.upload(process.cwd() + "/images/image_watermark.jpg")
-                        }
+                imageText = body.weather.cloud + "%\n" + body.weather.temperature + "°C\n" + body.weather.rain + "mm\n" + body.weather.fog + "%\n" + moment(body.date).format("Y-MM-DD HH:mm:ss Z") + "\n" + moment(body.weather.sunset).format("Y-MM-DD HH:mm:ss Z") + "\n" + body.weather.location.name + ", " + body.weather.location.state + " " + body.weather.location.country
+                // Cloud, temperature, rain, fog, time, sunset, location
+                iconText = '\uf013\n\uf055\n\uf017\n\uf014\n\uf08c\n\uf052\n\uf0eb'
+                gm(image).font(process.cwd() + "/weathericons-regular-webfont.ttf").fontSize(iconsize).stroke('black', 1).fill('white').drawText(25, 25, iconText, 'SouthWest').stroke('none').fill('white').drawText(25, 25, iconText, 'SouthWest').write(process.cwd() + "/images/image_watermark.jpg", function(err) {
+                    gm(process.cwd() + "/images/image_watermark.jpg").font(process.cwd() + "/OpenSans-Regular.ttf").fontSize(fontsize).stroke('black', 1).fill('white').drawText(85, 25, imageText, 'SouthWest').stroke('none').fill('white').drawText(85, 25, imageText, 'SouthWest').write(process.cwd() + "/images/image_watermark.jpg", function(err) {
+                        imports.logger.log("imagemagick", "Writing text to " + image)
+                        gm(process.cwd() + "/images/image_watermark.jpg").composite(process.cwd() + '/logo.png').gravity("SouthEast").geometry("+25+25").write(process.cwd() + "/images/image_watermark.jpg", function(err) {
+                            imports.logger.log("imagemagick", "Writing watermark to " + process.cwd() + "/images/image_watermark.jpg")
+                            if (err) {
+                                imports.logger.error("imagemagick", err)
+                            } else {
+                                imports.logger.log("imagemagick", "Image watermarked. Proceeding to upload")
+                                imports.uploader.upload(process.cwd() + "/images/image_watermark.jpg")
+                            }
+                        })
                     })
                 })
             })
